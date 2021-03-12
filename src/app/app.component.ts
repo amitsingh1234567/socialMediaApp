7a2997b65d286c6de4d9465c660b0752bcf99f4b
Amit singh <amitkumarpstc24@gmail.com>
2021-03-12 16:47:47 +0530

    keep scroll bottom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { ElementRef } from '@angular/core';
+import { ElementRef, Renderer2 } from '@angular/core';
 import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
@@ -11,17 +11,18 @@ export class AppComponent implements OnInit {
   @ViewChild('divStart') divStart: ElementRef;
   @ViewChild('side1') side1: ElementRef;
   @ViewChild('side2') side2: ElementRef;
-  @ViewChild('messages') messages: ElementRef;
+  @ViewChild('inputField') inputField: ElementRef;
+  @ViewChild('scrollMe') scrollDown: ElementRef;
 
+  reciverMessage:any;
 
-
-  // msg;
-  constructor() {
+  constructor(private renderer: Renderer2) {
 
   }
 
   ngOnInit() {
-
+    this.reciverMessage = [{"text": "This is text from anywhere"}, {"text": "This"},{"text": "This"},{"text": "This"},{"text": "This"},{"text": "This"},{"text": "This"},{"text": "This"}, {"text": "This"},{"text": "This"}, {"text": "This"}, {"text": "This"},{"text": "This"},{"text": "This"},{"text": "This"},{"text": "This"},{"text": "This"},{"text": "This"},{"text": "This"}];
+    // console.log(this.messages)
   }
 
   starCthat() {
@@ -31,49 +32,24 @@ export class AppComponent implements OnInit {
   }
 
   showChatList() {
-    this.side1.nativeElement.classList.remove('d-none', 'd-md-block')
+    this.side1.nativeElement.classList.remove('d-none', 'd-md-block');
     this.side2.nativeElement.classList.add('d-none');
   }
 
   hideChatList() {
-    this.side1.nativeElement.classList.add('d-none', 'd-md-block')
+    this.side1.nativeElement.classList.add('d-none', 'd-md-block');
     this.side2.nativeElement.classList.remove('d-none');
   }
 
   onKeydown(event) {
     if (event.key === "Enter") {
-      // console.log(event.target.value);
       this.sendMessage(event.target.value);
     }
   }
 
   sendMessage(value) {
-    // this.messages = message;
-    console.log(value)
-    let msg = `
-            <div class="row justify-content-end">
-            <div class="col-6 col-sm-7 col-md-7">
-              <p class="sent float-right">
-                This is text from anywhere
-              <span class="time float-right">1:28 PM</span>
-              </p>
-            </div>
-            <div class="col-2 col-sm-1 col-md-1">
-              <img src="../assets/pp.png" class="chat-pic" alt="..">
-            </div>
-          </div>
-    `
-
-    // this.messages.nativeElement.innerHTML = message
-    document.getElementById('messages').innerHTML += msg
-
+   this.reciverMessage.push({text: value});
+   this.inputField.nativeElement.value = '';
+   this.inputField.nativeElement.focus();
   }
-
-  // onKeyDowns(){
-  //   document.addEventListener('keydown', key => {
-  //     if(key.which === 13){
-
-  //     }
-  //   }) 
-  // }
 }
