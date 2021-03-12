997e6cf6a8651e2a4875099b5a37f2407aaf8763
Amit singh <amitkumarpstc24@gmail.com>
2021-03-12 17:02:57 +0530

    newChange

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,9 +21,7 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    // Hello World How R U
     this.reciverMessage = [{"text": "This is text from anywhere"}, {"text": "This"},{"text": "This"},{"text": "This"},{"text": "This"},{"text": "This"},{"text": "This"},{"text": "This"}, {"text": "This"},{"text": "This"}, {"text": "This"}, {"text": "This"},{"text": "This"},{"text": "This"},{"text": "This"},{"text": "This"},{"text": "This"},{"text": "This"},{"text": "This"}];
-    // console.log(this.messages)
   }
 
   starCthat() {
