724a4f930149674325eee97aca8e4b17fad1977b
Amit singh <amitkumarpstc24@gmail.com>
2021-03-13 11:13:08 +0530

    newFile

--- api/routes/authRoutes.js
+++
@@ -1,1 +0,0 @@
-//hbbdddnvsdnn
--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const app = express();
 
 
 const PORT = 3000;
-app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
